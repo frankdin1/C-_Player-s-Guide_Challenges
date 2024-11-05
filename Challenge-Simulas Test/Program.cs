8d0ce537045802ac800aaad70e8aa6596bcdb46e
frankdin <frankdin@yahoo.com>
2024-11-05 09:35:05 -0500

    Added challenge from Chapter 16

---
+++ Challenge-Simulas Test/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+BoxState box = BoxState.locked;
+string userAction = "";
+
+while (true)
+{
+    if (box == BoxState.locked)
+        while (userAction.ToLower() != "unlock")
+        {
+            Console.Write($"The chest is {box}. What do you want to do? ");
+            userAction = Console.ReadLine();
+        }
+
+    if (box == BoxState.unlocked)
+        while (userAction.ToLower() != "lock" && userAction.ToLower() != "open")
+        {
+            Console.Write($"The chest is {box}. What do you want to do? ");
+            userAction = Console.ReadLine();
+        }
+
+    if (box == BoxState.open)
+        while (userAction.ToLower() != "close")
+        {
+            Console.Write($"The chest is {box}. What do you want to do? ");
+            userAction = Console.ReadLine();
+        }
+
+    if (userAction.ToLower() == "unlock")
+        box = BoxState.unlocked;
+
+    if (userAction.ToLower() == "lock")
+        box = BoxState.locked;
+
+    if (userAction.ToLower() == "open")
+        box = BoxState.open;
+
+    if (userAction.ToLower() == "close")
+        box = BoxState.unlocked;
+}
+
+enum Action
+{
+    Unlock,
+    Open,
+    Close
+}
+
+enum BoxState
+{
+    open,
+    locked,
+    unlocked
+}
